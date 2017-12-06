71d9e62697987a005c7d59e115f044472df816ff
vinay thallam <vinay.thallam@jifflenow.com>
2017-12-06 23:38:03 +0530

    render method can return only single node of JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from 'react'
 
-const App = () => <h1>Hello stateless component</h1>
+class App extends React.Component {
+
+  render() {
+
+    // This will not work in react. Render method in class component is allowed to return only one node.
+    //return <h1>Hello Vinay</h1> <b>bold</b> 
+
+    //Wrap multile JSX nodes in a parent div
+    return (
+      <div>
+        <h1>Hello Vinay</h1>
+        <b>bold</b>
+      </div>
+      )
+  }
+}
 
 export default App
