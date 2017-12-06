65bcd319a208f8a218f24f4e071a9618571ff973
vinay thallam <vinay.thallam@jifflenow.com>
2017-12-06 23:44:00 +0530

    pass data to component using props

--- src/App.js
+++ src/App.js
@@ -4,16 +4,8 @@ class App extends React.Component {
 
   render() {
 
-    // This will not work in react. Render method in class component is allowed to return only one node.
-    //return <h1>Hello Vinay</h1> <b>bold</b> 
-
-    //Wrap multile JSX nodes in a parent div
-    return (
-      <div>
-        <h1>Hello Vinay</h1>
-        <b>bold</b>
-      </div>
-      )
+    let txt = this.props.txt
+    return <h1>{txt}</h1>
   }
 }
 
