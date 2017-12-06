6fe1948e6716a2abaaff26dd4d9d7f26128c5780
vinay thallam <vinay.thallam@jifflenow.com>
2017-12-06 23:26:28 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
-const App = () => <h1>Hello World !!</h1>
+class App extends React.Component {
+
+  render() {
+    return <h1>Hello Class Component</h1>
+  }
+}
 
 export default App
