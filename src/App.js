801f6ad81fd3675f1a4134679df87e7f40fc0aaa
vinay thallam <vinay.thallam@jifflenow.com>
2017-12-06 23:28:03 +0530

    JSX compiles to Javascript

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 class App extends React.Component {
 
   render() {
-    return <h1>Hello Class Component</h1>
+    return React.createElement('h1', null, 'Hello class component2')
   }
 }
 
