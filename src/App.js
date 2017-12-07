e817a89254f1f035c4c8c1bd4f23f8a8841d8342
vinay thallam <vinay.thallam@jifflenow.com>
2017-12-07 18:08:44 +0530

    custom prop validations - NOT WORKING i.e NOT SEEING error message in console

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Title txt="1234"/>
         <Widget update={this.update.bind(this)}/>
         <Widget update={this.update.bind(this)}/>
         <h1>{this.state.txt} - {this.state.cat}</h1>
@@ -26,5 +27,18 @@ class App extends React.Component {
 }
 
 const Widget = (props) => <input type="text" onChange={props.update} />
+
+const Title = (props) => <h1>{props.txt}</h1>
+
+Title.PropTypes = {
+  txt(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`Missing ${propName}`)
+    }
+    if(props[propName].length < 6){
+      return new Error(`${propName} too short`)
+    }
+  }
+}
 
 export default App
