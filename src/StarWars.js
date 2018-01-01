92a11ed853c96c065d3ca6f7f17b831da94e1d1f
vinay thallam <vinay.thallam@jifflenow.com>
2018-01-01 13:28:00 +0530

    fetching data from api and creating nodes with arrays of data

---
+++ src/StarWars.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class StarWars extends React.Component {
+
+	constructor(){
+		super()
+		this.state = {
+			items : []
+		}
+	}
+	componentWillMount(){
+		fetch('https://swapi.co/api/people/?format=json')
+		.then(response => response.json())
+		.then(function(data){
+			this.setState({items : data.results})
+		}.bind(this))
+		// .then(({results : items}) => this.setState(items))
+	}
+	render(){
+		let items = this.state.items
+
+		return (
+			<div>
+				{items.map(item => <h4 key={item.name}>{item.name}</h4>)}
+			</div>
+			)
+	}
+}
+
+export default StarWars
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Nested from './Nested';
+import StarWars from './StarWars';
 
 ReactDOM.render(<App txt="this is prop txt"/>, document.getElementById('root'));
 ReactDOM.render(<Nested />, document.getElementById('nested'));
+ReactDOM.render(<StarWars />, document.getElementById('starwars'));
